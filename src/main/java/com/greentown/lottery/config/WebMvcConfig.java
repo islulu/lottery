f29358422bdf320d11ac172210acdcaa7f6656bf
chenqihui <zjhzcqh@163.com>
2018-12-26 19:38:10 +0800

    修复String返回异常bug

--- src/main/java/com/greentown/lottery/config/WebMvcConfig.java
+++ src/main/java/com/greentown/lottery/config/WebMvcConfig.java
@@ -1,10 +1,8 @@
 package com.greentown.lottery.config;
 
-import com.alibaba.fastjson.serializer.SerializerFeature;
-import com.alibaba.fastjson.support.config.FastJsonConfig;
-import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -12,22 +10,9 @@ import java.util.List;
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
-    /**
-     * 配置消息转换器
-     * @param converters
-     */
     @Override
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-        FastJsonHttpMessageConverter fastJsonHttpMessageConverter = new FastJsonHttpMessageConverter();
-        //自定义配置...
-        FastJsonConfig config = new FastJsonConfig();
-        config.setSerializerFeatures(SerializerFeature.QuoteFieldNames,
-                SerializerFeature.WriteEnumUsingToString,
-                /*SerializerFeature.WriteMapNullValue,*/
-                SerializerFeature.WriteDateUseDateFormat,
-                SerializerFeature.DisableCircularReferenceDetect);
-        fastJsonHttpMessageConverter.setFastJsonConfig(config);
-        converters.add(fastJsonHttpMessageConverter);
-    }
 
+        converters.add(0, new MappingJackson2HttpMessageConverter());
+    }
 }
